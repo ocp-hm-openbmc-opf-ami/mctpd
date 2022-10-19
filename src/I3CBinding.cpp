c0e0ca7ab06d514cd7bb4eefcd5391d09732a479
Nidhin MS <nidhin.ms@intel.com>
2022-10-19 13:23:07 +0530

    Forward EID pool to endpoint (#99)
    Add support for a special case to send allocate eid command to the
    endpoint when SetEIDPool DBus method is called
    
    Tested:
    Steps:
    Reboot LBMC and NLBMC
    Simulated CPU will allocate EID pool to LBMC
    LBMC will distribute the pool to NLBMC and PFR service
    Check RX logs of MCTP_LBMC service in NLBMC console
    
    Verified AllocateEID pool command is received in NLBMC
    
    Signed-off-by: Nidhin MS <nidhin.ms@intel.com>

--- src/I3CBinding.cpp
+++ src/I3CBinding.cpp
@@ -15,7 +15,8 @@ I3CBinding::I3CBinding(std::shared_ptr<sdbusplus::asio::connection> conn,
     MctpBinding(conn, objServer, objPath, conf, ioc,
                 mctp_server::BindingTypes::MctpOverI3c),
     hw{std::move(hwParam)}, getRoutingInterval(conf.getRoutingInterval),
-    getRoutingTableTimer(ioc, getRoutingInterval), i3cConf(conf)
+    getRoutingTableTimer(ioc, getRoutingInterval), i3cConf(conf),
+    forwaredEIDPoolToEP(conf.forwaredEIDPoolToEP)
 {
     i3cInterface =
         objServer->add_interface(objPath, I3CBindingServer::interface);
@@ -655,4 +656,62 @@ std::vector<uint8_t> I3CBinding::getOwnPhysicalAddress()
 {
     // Update proper physical address
     return std::vector<uint8_t>{hw->getOwnAddress()};
+}
+
+bool I3CBinding::setEIDPool(const uint8_t startEID, const uint8_t poolSize)
+{
+    if (!MctpBinding::setEIDPool(startEID, poolSize))
+    {
+        return false;
+    }
+    if (this->forwaredEIDPoolToEP)
+    {
+        boost::asio::spawn(
+            this->connection->get_io_context(),
+            [this, startEID, poolSize](boost::asio::yield_context yield) {
+                this->forwardEIDPool(yield, startEID, poolSize);
+            });
+    }
+    return true;
+}
+
+void I3CBinding::forwardEIDPool(boost::asio::yield_context& yield,
+                                const uint8_t startEID, const uint8_t poolSize)
+{
+    std::vector<uint8_t> resp;
+    if (!this->allocateEIDPoolCtrlCmd(
+            yield, MCTP_EID_NULL,
+            mctp_ctrl_cmd_allocate_eids_req_op::allocate_eids, startEID,
+            poolSize, resp))
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Error while sending Allocate EID during forward eid pool");
+        return;
+    }
+
+    mctp_ctrl_cmd_allocate_eids_resp respData;
+    mctp_ctrl_cmd_allocate_eids_resp_op op;
+    if (!mctp_decode_ctrl_cmd_allocate_endpoint_id_resp(
+            reinterpret_cast<mctp_ctrl_cmd_allocate_eids_resp*>(resp.data()),
+            &respData.ctrl_hdr.ic_msg_type, &respData.ctrl_hdr.rq_dgram_inst,
+            &respData.ctrl_hdr.command_code, &respData.completion_code, &op,
+            &respData.eid_pool_size, &respData.first_eid))
+    {
+
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Allocate EID decode error");
+        return;
+    }
+    if (respData.completion_code != MCTP_CTRL_CC_SUCCESS)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Allocate EID was not succesful");
+        return;
+    }
+    if (respData.operation !=
+        mctp_ctrl_cmd_allocate_eids_resp_op::allocation_accepted)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Allocate EID rejected by the endpoint");
+    }
 }
